5faac817f3c2f17ec24b81ab0a41881ac510cc61
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-10 21:57:16 +0700

    Create dummy data for previews

--- AsyncAwaitMVVM/Features/Quotes/Model/Quote.swift
+++ AsyncAwaitMVVM/Features/Quotes/Model/Quote.swift
@@ -12,3 +12,13 @@ struct Quote: Decodable {
     let character: String
     let quote: String
 }
+
+extension Quote {
+    static let dummyDate: [Quote] = [
+        Quote(anime: "Anime 1", character: "Character 1", quote: "Random Quote 1"),
+        Quote(anime: "Anime 2", character: "Character 2", quote: "Random Quote 2"),
+        Quote(anime: "Anime 3", character: "Character 3", quote: "Random Quote 3"),
+        Quote(anime: "Anime 4", character: "Character 4", quote: "Random Quote 4"),
+        Quote(anime: "Anime 5", character: "Character 5", quote: "Random Quote 5")
+    ]
+}

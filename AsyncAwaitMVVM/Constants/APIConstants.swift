0fe3ff630f50f6283d79c1aa20c3f537874a716b
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-10 21:54:36 +0700

    Create a constant folder

---
+++ AsyncAwaitMVVM/Constants/APIConstants.swift
@@ -0,0 +1,12 @@
+//
+//  APIConstants.swift
+//  AsyncAwaitMVVM
+//
+//  Created by Danh Tu on 10/10/2021.
+//
+
+import Foundation
+
+enum APIConstants {
+    static let baseUrl = "https://animechan.vercel.app"
+}

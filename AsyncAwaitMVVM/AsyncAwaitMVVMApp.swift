1531c6d4cafd6dcc3ce1fd9d7a7437333ef2c145
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-10 21:53:05 +0700

    Create the project

---
+++ AsyncAwaitMVVM/AsyncAwaitMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitMVVMApp.swift
+//  AsyncAwaitMVVM
+//
+//  Created by Danh Tu on 10/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

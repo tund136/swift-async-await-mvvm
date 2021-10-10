b495ab52f4934f2c9f841fb743165dd9ff2d088f
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-10 21:56:03 +0700

    Design Quote Model

---
+++ AsyncAwaitMVVM/Features/Quotes/Model/Quote.swift
@@ -0,0 +1,14 @@
+//
+//  Quote.swift
+//  AsyncAwaitMVVM
+//
+//  Created by Danh Tu on 10/10/2021.
+//
+
+import Foundation
+
+struct Quote: Decodable {
+    let anime: String
+    let character: String
+    let quote: String
+}
